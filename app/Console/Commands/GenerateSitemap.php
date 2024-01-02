5d2d9d2a55d0ea2a2cb747cf68b639f633064132
gcazin <czn.guillaume@gmail.com>
2024-01-02 21:38:15 +0100

    feat: add sitemap

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Sitemap generation in progress...');
+
+        SitemapGenerator::create('https://x-memes.com')
+            ->configureCrawler(function ($crawler) {
+                $crawler->ignoreRobots();
+            })
+            ->writeToFile(public_path('sitemap.xml'));
+
+        $this->info('Sitemap generation complete !');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('telescope:prune')->daily();
+        $schedule->command('sitemap:generate')->daily();
     }
 
     /**
