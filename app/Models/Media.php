259c329f24c88be8d2fff2e8ba407a5fea6e3864
gcazin <czn.guillaume@gmail.com>
2024-01-18 21:42:51 +0100

    tests

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -34,7 +34,7 @@ class Media extends Model implements Sitemapable
     protected $table = 'medias';
 
     protected $with = [
-        'user',
+//        'user',
 //        'tags',
 //        'likers',
     ];
