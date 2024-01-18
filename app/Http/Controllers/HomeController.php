d533b55f5df8a92ac87e82d2fa5b0f951a539a1d
gcazin <czn.guillaume@gmail.com>
2024-01-18 22:21:45 +0100

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
 {
     public function __invoke(): Response|RedirectResponse
     {
-        dd(Media::all());
+        dd(Media::all()->load('user'));
         if (auth()->user()) {
             return redirect()->to(RouteServiceProvider::LIBRARY);
         }
