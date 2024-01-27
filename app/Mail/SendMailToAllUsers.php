f6cab41fc25e074ccf49c04435b61ba1fb468eca
gcazin <czn.guillaume@gmail.com>
2024-01-27 14:40:05 +0100

    fix: tests

--- app/Mail/SendMailToAllUsers.php
+++ app/Mail/SendMailToAllUsers.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
