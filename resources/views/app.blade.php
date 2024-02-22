28d9ddc26e57cb22362ade1a2cea76380827706c
gcazin <czn.guillaume@gmail.com>
2024-02-22 23:44:18 +0100

    ref: move tarteaucitron to the head bottom

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,56 +11,6 @@
 
         <link rel="manifest" href="{{ asset('xmemes.webmanifest') }}">
 
-        <!-- Tarteaucitron -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tarteaucitronjs/1.16.1/tarteaucitron.min.js"></script>
-        <script type="text/javascript">
-            tarteaucitron.init({
-                "privacyUrl": "", /* Privacy policy url */
-                "bodyPosition": "bottom", /* or top to bring it as first element for accessibility */
-
-                "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
-                "cookieName": "tarteaucitron", /* Cookie name */
-
-                "orientation": "bottom", /* Banner position (top - bottom) */
-
-                "groupServices": false, /* Group services by category */
-                "showDetailsOnClick": true, /* Click to expand the description */
-                "serviceDefaultState": true, /* Default state (true - wait - false) */
-
-                "showAlertSmall": false, /* Show the small banner on bottom right */
-                "cookieslist": false, /* Show the cookie list */
-
-                "closePopup": false, /* Show a close X on the banner */
-
-                "showIcon": true, /* Show cookie icon to manage cookies */
-                //"iconSrc": "", /* Optionnal: URL or base64 encoded image */
-                "iconPosition": "BottomRight", /* BottomRight, BottomLeft, TopRight and TopLeft */
-
-                "adblocker": false, /* Show a Warning if an adblocker is detected */
-
-                "DenyAllCta" : true, /* Show the deny all button */
-                "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
-                "highPrivacy": true, /* HIGHLY RECOMMANDED Disable auto consent */
-
-                "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
-
-                "removeCredit": false, /* Remove credit link */
-                "moreInfoLink": true, /* Show more info link */
-
-                "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
-                "useExternalJs": false, /* If false, the tarteaucitron.js file will be loaded */
-
-                //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
-
-                "readmoreLink": "", /* Change the default readmore link */
-
-                "mandatory": true, /* Show a message about mandatory cookies */
-                "mandatoryCta": true, /* Show the disabled accept button when mandatory on */
-
-                //"customCloserId": "" /* Optional a11y: Custom element ID used to open the panel */
-            });
-        </script>
-
         <!-- Icons set -->
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
 
@@ -72,6 +22,39 @@
         @routes
         @vite('resources/js/app.js')
         @inertiaHead
+
+        <!-- Tarteaucitron -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tarteaucitronjs/1.16.1/tarteaucitron.min.js"></script>
+        <script type="text/javascript">
+            tarteaucitron.init({
+                "privacyUrl": "", /* Privacy policy url */
+                "bodyPosition": "bottom", /* or top to bring it as first element for accessibility */
+                "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+                "cookieName": "tarteaucitron", /* Cookie name */
+                "orientation": "bottom", /* Banner position (top - bottom) */
+                "groupServices": false, /* Group services by category */
+                "showDetailsOnClick": true, /* Click to expand the description */
+                "serviceDefaultState": true, /* Default state (true - wait - false) */
+                "showAlertSmall": false, /* Show the small banner on bottom right */
+                "cookieslist": false, /* Show the cookie list */
+                "closePopup": false, /* Show a close X on the banner */
+                "showIcon": true, /* Show cookie icon to manage cookies */
+                //"iconSrc": "", /* Optionnal: URL or base64 encoded image */
+                "iconPosition": "BottomRight", /* BottomRight, BottomLeft, TopRight and TopLeft */
+                "adblocker": false, /* Show a Warning if an adblocker is detected */
+                "DenyAllCta" : true, /* Show the deny all button */
+                "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+                "highPrivacy": true, /* HIGHLY RECOMMANDED Disable auto consent */
+                "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+                "removeCredit": false, /* Remove credit link */
+                "moreInfoLink": true, /* Show more info link */
+                "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+                "useExternalJs": false, /* If false, the tarteaucitron.js file will be loaded */
+                "readmoreLink": "", /* Change the default readmore link */
+                "mandatory": true, /* Show a message about mandatory cookies */
+                "mandatoryCta": true, /* Show the disabled accept button when mandatory on */
+            });
+        </script>
     </head>
     <body class="font-sans antialiased !mb-0">
         @inertia
