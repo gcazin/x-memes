386e80e6d42668f7cb47cd48703bc24572e2740f
gcazin <czn.guillaume@gmail.com>
2024-03-13 17:22:32 +0100

    feat: add post test

--- app/Repositories/TagRepository.php
+++ app/Repositories/TagRepository.php
@@ -6,7 +6,7 @@ namespace App\Repositories;
 
 use App\Interfaces\RepositoryInterface;
 use App\Models\Tag;
-use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Collection;
 
 class TagRepository implements RepositoryInterface
 {
---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+
+it('can see posts index', function () {
+    User::factory()->create();
+    $response = actingAsGuest()->get(route('post.index'));
+
+    expect($response->status())->toBe(200);
+});
+
+it('can see post', function() {
+    User::factory()->create();
+    $post = Post::factory()->create([
+        'is_draft' => false
+    ]);
+
+    $response = actingAsGuest()->get(route('post.show', $post->slug));
+
+    expect($response->status())->toBe(200);
+});
+
+it('cannot see unpublished post', function() {
+    User::factory()->create();
+    $post = Post::factory()->create([
+        'is_draft' => true
+    ]);
+
+    $response = actingAsGuest()->get(route('post.show', $post->slug));
+
+    expect($response->status())->toBe(404);
+});
